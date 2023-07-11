c76cc94ffe7e2d9ec618aeb8e883e03b44f9086e
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-07-11 23:13:14 +0300

    LbControllersAndViewAdd

---
+++ TahilBorsaMS/Controllers/LabaratuarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TahilBorsaMS.Controllers;
+using TahilBorsaMS.Models.Entity;
+
+namespace TahilBorsaMS.Controllers
+{
+    public class LabaratuarController : Controller
+    {
+        DbGrainExchangeEntities db = new DbGrainExchangeEntities();
+        // GET: Labaratuar
+        public ActionResult Index()
+        {
+            var lab = db.tblLabData.ToList();
+            return View(lab);
+        }
+    }
+}
