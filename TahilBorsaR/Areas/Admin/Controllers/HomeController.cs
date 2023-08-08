b8b0bd18d32fc495594de4488012da4eb4d0efcb
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-08-08 19:23:38 +0300

    Add_Project_WithJqueryAjax

---
+++ TahilBorsaR/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TahilBorsaR.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Tumsehirler()
+        {
+            return View();
+        }
+    }
+}
