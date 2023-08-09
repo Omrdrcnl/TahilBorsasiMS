aee7f4ddbb90fabd0de8772dcf4a442c1c56f243
Ömer DİRİCANLI <omrdrcnl@gmail.com>
2023-08-09 14:57:02 +0300

    ChangeFolderName

---
+++ TahilBorsaJqueryAjax/Areas/Admin/Controllers/FarmerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TahilBorsaJqeryAjax.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FarmerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddFarmer()
+        {
+            return View();
+        }
+    }
+}
